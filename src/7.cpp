ef8cd2582cf35195d87228f7ee8b0bca9d5a17d9
lazsercho926 <lazsercho926@gmail.com>
2025-03-11 16:06:55 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "Alice";
+    int age = 25;
+    cout << "My name is " << name << " and I am " << age << " years old." << endl;
+    return 0;
+}
