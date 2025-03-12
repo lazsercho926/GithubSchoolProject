d5d260a86e632e3570381a1a33464ec7ad2e104c
lazsercho926 <lazsercho926@gmail.com>
2025-03-12 03:58:16 +0000

    Add 8.cpp

---
+++ src/8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x = 10;
+  double y = 3.5;
+  char z = 'a';
+  cout << "x: " << x << endl;
+  cout << "y: " << y << endl;
+  cout << "z: " << z << endl;
+  return 0;
+}
