f9cbee82ea9565924990e4d123299cc1ce02881c
lazsercho926 <lazsercho926@gmail.com>
2025-04-27 07:26:44 +0000

    Add 36.cpp

---
+++ src/36.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1 = 5;
+    int number2 = 3;
+    int sum = number1 + number2;
+    cout << "The sum of " << number1 << " and " << number2 << " is " << sum << endl;
+    return 0;
+}
